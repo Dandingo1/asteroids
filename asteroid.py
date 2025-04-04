35d9f5565f1f93498cfb5d13145414e0311660f2
Dandingo <dedominguez97@gmail.com>
2025-04-04 16:26:08 -0400

    feat(split): Added functionality to split asteroids on collide with bullet

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
+import random
 import pygame
 import circleshape
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(circleshape.CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,19 @@ class Asteroid(circleshape.CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        random_angle = random.uniform(20, 50)
+        velocity_one = self.velocity.rotate(random_angle)
+        velocity_two = self.velocity.rotate(-random_angle)
+
+        asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_one = Asteroid(self.position.x, self.position.y, asteroid_radius)
+        asteroid_two = Asteroid(self.position.x, self.position.y, asteroid_radius)
+
+        asteroid_one.velocity = velocity_one * 1.2
+        asteroid_two.velocity = velocity_two * 1.2
